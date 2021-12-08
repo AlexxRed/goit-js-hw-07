b4c05ad94cec1ea5191aa603b2feb9def93281d0
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-08 22:15:55 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onImageClick)
+
+function createImagesMarkup(items) {
+    return items.map(item => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    />
+    </a>
+</div>`
+    ).join('')
+};
+
+const insertMarkup =  createImagesMarkup(galleryItems)
+
+galleryRef.innerHTML = insertMarkup
+
+function onImageClick(e) {
+    e.preventDefault();
+    
+}
