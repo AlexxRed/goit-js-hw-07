97606b7877ddd69d01a5bab9c51eae41b797aa36
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-09 16:02:36 +0200

    task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryRef = document.querySelector('.gallery');
 
-galleryRef.addEventListener('click', onImageClick)
+galleryRef.addEventListener('click', onImageClick);
 
 function createImagesMarkup(items) {
     return items.map(item => 
@@ -22,11 +20,37 @@ function createImagesMarkup(items) {
     ).join('')
 };
 
-const insertMarkup =  createImagesMarkup(galleryItems)
+const insertMarkup = createImagesMarkup(galleryItems);
 
-galleryRef.innerHTML = insertMarkup
+galleryRef.innerHTML = insertMarkup;
 
 function onImageClick(e) {
     e.preventDefault();
-    
+
+    if (e.target.className !== 'gallery__image') {return};
+
+    const selectedImage = e.target.dataset.source;
+    const selectedAlt = e.target.attributes.alt.nodeValue
+    const minImage = e.target.attributes.src.nodeValue
+
+    const instance = basicLightbox.create(`
+    <img
+    class="gallery__image-open"    
+    src="${selectedImage}"
+    alt="${selectedAlt}"
+    data-source="${minImage}"
+    />
+    `);
+
+    if (!basicLightbox.visible()) {
+        instance.show();
+        window.addEventListener('keydown', closeModal);
+        
+    function closeModal(e) {
+        if (e.code === 'Escape') {
+            instance.close();
+            window.removeEventListener('keydown', closeModal);
+        };
+    }
+    } 
 }
