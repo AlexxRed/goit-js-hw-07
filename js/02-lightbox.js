0b286971beb40080d76eb6d129a8e36c79f1a4aa
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-09 16:47:07 +0200

    task_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onImageClick);
+
+function createImagesMarkup(items) {
+    return items.map(item => 
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    ).join('')
+};
+
+const insertMarkup = createImagesMarkup(galleryItems);
+
+galleryRef.innerHTML = insertMarkup;
+
+function onImageClick(e) {
+    e.preventDefault();
+    if (e.target.className !== 'gallery__image') { return };
+    
+    new SimpleLightbox('.gallery__item', {
+        captions: true,
+        captionSelector: 'img',
+        captionType: 'attr',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    });
+}
